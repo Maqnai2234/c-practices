df65c3d63324e519c348e3d1b4f9bafc41d11267
Alexei <alexeim763@gmail.com>
2018-05-27 13:40:58 -0500

    fixed

--- sum.cpp
+++ sum.cpp
@@ -3,11 +3,8 @@ using namespace std;
 int main(){
     int number1 = 0, number2 = 0, sum = 0;
 
-    cout<< "Enter first Integer: ";
-    cin >> number1;
-
-    cout<< "Enter second Integer: ";
-    cin >> number2;
+    cout<< "Enter two Integers: ";
+    cin >> number1 >> number2;
 
     sum = number1 + number2;
 
