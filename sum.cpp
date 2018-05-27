c35e20daa5a2a44ed560b6e011b6bef19ba106cb
Alexei <alexeim763@gmail.com>
2018-05-27 13:38:43 -0500

    ...

--- sum.cpp
+++ sum.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-
+using namespace std;
 int main(){
     int number1 = 0, number2 = 0, sum = 0;
 
-    std::cout<< "Enter first Integer: ";
-    std::cin >> number1;
+    cout<< "Enter first Integer: ";
+    cin >> number1;
 
-    std::cout<< "Enter second Integer: ";
-    std::cin >> number2;
+    cout<< "Enter second Integer: ";
+    cin >> number2;
 
     sum = number1 + number2;
 
-    std::cout << "Sum is " << sum << std::endl;
+    cout << "Sum is " << sum << endl;
 
 }
