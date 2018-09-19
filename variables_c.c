9bc21856b52b6cbdff9e73d33ffa9211d82c0b2e
Alexei <alexeim763@gmail.com>
2018-09-18 22:08:33 -0500

    add new varaible

---
+++ variables_c.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+ #define numeroConst 5
+
+void main()
+{
+  int numeroVar = 10;
+  int resultado;
+  
+  resultado = numeroConst + numeroVar;
+
+  printf("El Resultado es: %d", resultado);
+}
+
